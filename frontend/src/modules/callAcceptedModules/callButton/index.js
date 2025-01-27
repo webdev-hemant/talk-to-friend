8e8b5bc8aa313420c5b0eb6a53d64ee5c024d3db
Hemant Jadhav <hemant.jadhav@navgrahaa.local>
2025-01-27 15:54:58 +0530

    changes

--- frontend/src/modules/callAcceptedModules/callButton/index.js
+++ frontend/src/modules/callAcceptedModules/callButton/index.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { MdCallEnd } from "react-icons/md";
+import { BiSolidCameraMovie } from "react-icons/bi";
+import { FaMicrophoneSlash } from "react-icons/fa";
 import styles from "./callButton.module.scss";
 
 const CallButton = () => {
-  return <div className={styles.callBtnWrapper}>CallButton</div>;
+  return (
+    <div className={styles.callBtnWrapper}>
+      <ul className={styles.listContainer}>
+        <li>
+          <BiSolidCameraMovie />
+        </li>
+        <li>
+          <MdCallEnd />
+        </li>
+        <li>
+          <FaMicrophoneSlash />
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default CallButton;
